d702a137a646ac7b16ad9ebc98c352cab2b60cc3
Josephmar Diaz <josephmardiaz@gmail.com>
2023-06-29 11:06:52 -0400

    agregada user_id

--- database/migrations/2023_06_27_152212_create_products_table.php
+++ database/migrations/2023_06_27_152212_create_products_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->string('name');
             $table->string('country');
             $table->unsignedBigInteger('document_id');
