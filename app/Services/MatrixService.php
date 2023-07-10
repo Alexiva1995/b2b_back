6285ffa4932201d3928f2253c731672bd8f8c7d4
Cesar Febres <febrescesar7@gmail.com>
2023-07-10 15:08:48 -0400

    level 3

--- app/Services/MatrixService.php
+++ app/Services/MatrixService.php
@@ -14,27 +14,18 @@ class MatrixService
         $users = $users->where('matrix_level', '!=', 4);
 
         foreach ($users as $user) {
-            if ($user->matrix_level == 0) {
-                $userRight = $users->where('buyer_id', $user->id)->where('binary_side', 'R')->first();
-                $userLeft = $users->where('buyer_id', $user->id)->where('binary_side', 'L')->first();
-                if ($userLeft && $userRight) {
-                    $user->update(['matrix_level' => 1]);
-                    $this->levelTwo($user, $users, $userLeft, $userRight);
-                }
-            } else if ($user->matrix_level == 1) {
+            $userRight = $users->where('buyer_id', $user->id)->where('binary_side', 'R')->first();
+            $userLeft = $users->where('buyer_id', $user->id)->where('binary_side', 'L')->first();
+            if ($userLeft && $userRight) {
 
-                $userRight = $users->where('buyer_id', $user->id)->where('binary_side', 'R')->first();
-                $userLeft = $users->where('buyer_id', $user->id)->where('binary_side', 'L')->first();
+                if($user->matrix_level < 1)  $user->update(['matrix_level' => 1]);
+
                 $this->levelTwo($user, $users, $userLeft, $userRight);
-            } else if($user->matrix_level == 2) {
-                // Logica para nivel 3
-            } else if($user->matrix_level == 3){
-                // Logica para nivel 4
             }
         }
     }
 
-    public function levelTwo(User $currentUser, $users , User $userLeft, User $userRight)
+    public function levelTwo(User $currentUser, $users, User $userLeft, User $userRight)
     {
         $user_left_left = $users->where('buyer_id', $userLeft->id)->where('binary_side', 'L')->first();
         $user_left_right = $users->where('buyer_id', $userLeft->id)->where('binary_side', 'R')->first();
@@ -42,14 +33,38 @@ class MatrixService
         $user_right_left = $users->where('buyer_id', $userRight->id)->where('binary_side', 'L')->first();
         $user_right_right = $users->where('buyer_id', $userRight->id)->where('binary_side', 'R')->first();
 
-        if($user_left_left && $user_left_right && $user_right_left && $user_right_right) {
-            $current_user->update(['matrix_level' => 2]);
-            $this->levelThree();
+        if ($user_left_left && $user_left_right && $user_right_left && $user_right_right) {
+
+            if($currentUser->matrix_level < 2) $currentUser->update(['matrix_level' => 2]);
+
+            $this->levelThree($currentUser, $users, $user_left_left, $user_left_right, $user_right_left, $user_right_right);
         }
     }
 
-    public function levelThree()
+    public function levelThree(User $currentUser, $users, User $user_left_left, User $user_left_right, User $user_right_left,  User $user_right_right)
     {
+        $user_left_left_left = $users->where('buyer_id', $user_left_left->id)->where('binary_side', 'L')->first();
+        $user_left_left_right = $users->where('buyer_id', $user_left_left->id)->where('binary_side', 'R')->first();
 
+        $user_left_right_left = $users->where('buyer_id', $user_left_right->id)->where('binary_side', 'L')->first();
+        $user_left_right_right = $users->where('buyer_id', $user_left_right->id)->where('binary_side', 'R')->first();
+
+        $user_right_left_left = $users->where('buyer_id', $user_right_left->id)->where('binary_side', 'L')->first();
+        $user_right_left_right = $users->where('buyer_id', $user_right_left->id)->where('binary_side', 'R')->first();
+
+        $user_right_right_left = $users->where('buyer_id', $user_right_right->id)->where('binary_side', 'L')->first();
+        $user_right_right_right = $users->where('buyer_id', $user_right_right->id)->where('binary_side', 'R')->first();
+
+        if($user_left_left_left && $user_left_left_right && $user_left_right_left && $user_left_right_right && $user_right_left_left && $user_right_left_right && $user_right_right_left && $user_right_right_right)
+        {
+            if($currentUser->matrix_level < 3) $currentUser->update(['matrix_level' => 3]);
+
+            $this->levelFour();
+        }
+    }
+
+    public function levelFour()
+    {
+        
     }
 }
