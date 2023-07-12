852e177ca7db846351ec234b5e1cba2c6c8029a4
Eduardo Fonseca <eduardofonseca@valdusoft.com>
2023-07-12 15:11:39 -0400

    create relations

--- app/Models/MarketPurchased.php
+++ app/Models/MarketPurchased.php
@@ -10,4 +10,19 @@ class MarketPurchased extends Model
     use HasFactory;
 
     protected $fillable = ['user_id', 'cyborg_id', 'order_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function cyborg()
+    {
+        return $this->belongsTo(Market::class, 'cyborg_id');
+    }
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
 }
