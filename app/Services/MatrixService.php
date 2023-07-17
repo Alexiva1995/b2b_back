b773ad0815b787dcad198be7fd2d597a611a4e73
LuisA-P <luisprieto@valdusoft.com>
2023-07-17 14:36:58 -0400

    Finish change type matrix

--- app/Services/MatrixService.php
+++ app/Services/MatrixService.php
@@ -4,6 +4,7 @@ namespace App\Services;
 
 use App\Models\MarketPurchased;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class MatrixService
 {
@@ -13,7 +14,7 @@ class MatrixService
     {
         $this->bonusService = $bonusService;
     }
-
+    // Reto a quien quiera intentar optimizar este codigo haciendo una funcion recursiva y dinamica, si lo logras, pide un aumento 
     public function levelOne()
     {
         // Obtenemos a todos los usuarios que no tengan nivel maximo y que no sean admin.
@@ -21,12 +22,12 @@ class MatrixService
 
         foreach ($users as $user) {
             foreach ($user->marketPurchased as $matrixPurchased) {
-                $userRight = $users->where('father_cyborg_purchased_id', $matrixPurchased->id)->where('binary_side', 'R')->where('buyer_id', $user->id)->where('status', User::ACTIVE)->whereHas('marketPurchased', function ($q, $matrixPurchased) {
+                $userRight = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
                     $q->where('type', '>=', $matrixPurchased->type);
-                })->first();
-                $userLeft = $users->where('father_cyborg_purchased_id', $matrixPurchased->id)->where('binary_side', 'L')->where('buyer_id', $user->id)->where('status', User::ACTIVE)->whereHas('marketPurchased', function ($q, $matrixPurchased) {
+                })->where('father_cyborg_purchased_id', $matrixPurchased->id)->where('binary_side', 'R')->where('buyer_id', $user->id)->where('status', User::ACTIVE)->first();
+                $userLeft = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
                     $q->where('type', '>=', $matrixPurchased->type);
-                })->first();
+                })->where('father_cyborg_purchased_id', $matrixPurchased->id)->where('binary_side', 'L')->where('buyer_id', $user->id)->where('status', User::ACTIVE)->first();
                 if ($userLeft && $userRight) {
 
                     if ($matrixPurchased->level < 1) {
@@ -42,18 +43,27 @@ class MatrixService
 
     public function levelTwo(MarketPurchased $matrixPurchased, $users, User $userLeft, User $userRight)
     {
-        $user_left_left = $users->where('buyer_id', $userLeft->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_right = $users->where('buyer_id', $userLeft->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+            $q->where('type', '>=', $matrixPurchased->type);
+        })->where('buyer_id', $userLeft->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+            $q->where('type', '>=', $matrixPurchased->type);
+        })->where('buyer_id', $userLeft->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_left = $users->where('buyer_id', $userRight->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_right = $users->where('buyer_id', $userRight->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+            $q->where('type', '>=', $matrixPurchased->type);
+        })->where('buyer_id', $userRight->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+            $q->where('type', '>=', $matrixPurchased->type);
+        })->where('buyer_id', $userRight->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
         if ($user_left_left && $user_left_right && $user_right_left && $user_right_right) {
 
             if ($matrixPurchased->level < 2) {
                 $matrixPurchased->level = 2;
                 $matrixPurchased->save();
-                $this->bonusService->subtract($amount = 50, $matrixPurchased->user_id, $matrixPurchased->cyborg->id, $level = 2);
+                $amount = $matrixPurchased->type == MarketPurchased::MATRIX_20 ? 50 : ($matrixPurchased->type == MarketPurchased::MATRIX_200 ? 500 : 5_000);
+                $this->bonusService->subtract($amount, $matrixPurchased->user_id, $matrixPurchased->cyborg->id, $level = 2);
             }
 
             $this->levelThree($matrixPurchased, $users, $user_left_left, $user_left_right, $user_right_left, $user_right_right);
@@ -62,24 +72,41 @@ class MatrixService
 
     public function levelThree(MarketPurchased $matrixPurchased, $users, User $user_left_left, User $user_left_right, User $user_right_left, User $user_right_right)
     {
-        $user_left_left_left = $users->where('buyer_id', $user_left_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_left_right = $users->where('buyer_id', $user_left_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_left_right_left = $users->where('buyer_id', $user_left_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_right_right = $users->where('buyer_id', $user_left_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_left_left = $users->where('buyer_id', $user_right_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_left_right = $users->where('buyer_id', $user_right_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_right_left = $users->where('buyer_id', $user_right_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_right_right = $users->where('buyer_id', $user_right_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
         if ($user_left_left_left && $user_left_left_right && $user_left_right_left && $user_left_right_right && $user_right_left_left && $user_right_left_right && $user_right_right_left && $user_right_right_right) {
 
             if ($matrixPurchased->level < 3) {
                 $matrixPurchased->level = 3;
                 $matrixPurchased->save();
-                $this->bonusService->subtract($amount = 100, $matrixPurchased->user_id, $matrixPurchased->cyborg->id, $level = 3);
+                $amount = $matrixPurchased->type == MarketPurchased::MATRIX_20 ? 100 : ($matrixPurchased->type == MarketPurchased::MATRIX_200 ? 1_000 : 10_000);
+                $this->bonusService->subtract($amount, $matrixPurchased->user_id, $matrixPurchased->cyborg->id, $level = 3);
             }
 
             $this->levelFour(
@@ -99,29 +126,61 @@ class MatrixService
 
     public function levelFour(MarketPurchased $matrixPurchased, $users, User $user_left_left_left, User $user_left_left_right, User $user_left_right_left, User $user_left_right_right, User $user_right_left_left, User $user_right_left_right, User $user_right_right_left, User $user_right_right_right)
     {
-        $user_left_left_left_left = $users->where('buyer_id', $user_left_left_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_left_left_right = $users->where('buyer_id', $user_left_left_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_left_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_left_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_left_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_left_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_left_left_right_left = $users->where('buyer_id', $user_left_left_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_left_right_right = $users->where('buyer_id', $user_left_left_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_left_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_left_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_left_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_left_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_left_right_left_left = $users->where('buyer_id', $user_left_right_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_right_left_right = $users->where('buyer_id', $user_left_right_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_right_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_right_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_right_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_right_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_left_right_right_left = $users->where('buyer_id', $user_left_right_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_left_right_right_right = $users->where('buyer_id', $user_left_right_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_left_right_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_right_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_left_right_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_left_right_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_left_left_left = $users->where('buyer_id', $user_right_left_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_left_left_right = $users->where('buyer_id', $user_right_left_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_left_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_left_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_left_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_left_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_left_right_left = $users->where('buyer_id', $user_right_left_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_left_right_right = $users->where('buyer_id', $user_right_left_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_left_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_left_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_left_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_left_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_right_left_left = $users->where('buyer_id', $user_right_right_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_right_left_right = $users->where('buyer_id', $user_right_right_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_right_left_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_right_left->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_right_left_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_right_left->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
-        $user_right_right_right_left = $users->where('buyer_id', $user_right_right_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
-        $user_right_right_right_right = $users->where('buyer_id', $user_right_right_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
+        $user_right_right_right_left = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_right_right->id)->where('binary_side', 'L')->where('status', User::ACTIVE)->first();
+        $user_right_right_right_right = User::whereHas('marketPurchased', function ($q) use($matrixPurchased) {
+                    $q->where('type', '>=', $matrixPurchased->type);
+                })->where('buyer_id', $user_right_right_right->id)->where('binary_side', 'R')->where('status', User::ACTIVE)->first();
 
         if (
             $user_left_left_left_left && $user_left_left_left_right && $user_left_left_right_left && $user_left_left_right_right && $user_left_right_left_left && $user_left_right_left_right
@@ -129,9 +188,12 @@ class MatrixService
             && $user_right_left_right_right && $user_right_right_left_left && $user_right_right_left_right && $user_right_right_right_left && $user_right_right_right_right
         ) {
 
-            if ($matrixPurchased->level < 4) {
+            if ($matrixPurchased->level < 4 && $matrixPurchased->type < MarketPurchased::MATRIX_2000) {
                 $matrixPurchased->level = 0;
-                $matrixPurchased->type = MarketPurchased::MATRIX_200;
+                $matrixPurchased->type = $matrixPurchased->type + 1;
+                $matrixPurchased->save();
+            } else {
+                $matrixPurchased->level = 4; 
                 $matrixPurchased->save();
             }
         }
