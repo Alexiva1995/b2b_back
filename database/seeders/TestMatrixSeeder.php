69b5889d8fdbac66deb681902851adbbc7902304
Eduardo Fonseca <eduardofonseca@valdusoft.com>
2023-08-09 22:18:36 -0400

    change seeder

--- database/seeders/TestMatrixSeeder.php
+++ database/seeders/TestMatrixSeeder.php
@@ -23,22 +23,22 @@ class TestMatrixSeeder extends Seeder
      */
     public function run()
     {
-        Package::create([
+       /*  Package::create([
             'package' => 'Test',
             'description' => 'Hi i am a description',
             'gain' => 50,
             'amount' => 50,
             'type' => 0,
             'level' => 1
-        ]);
+        ]); */
 
         // admin
         $bonusService = new BonusService;
 
       $admin=  User::create([
-            'name'=> 'user',
-            'last_name'=> 'admin',
-            'email'=> 'admin@b2b.com',
+            'name'=> 'Admin',
+            'last_name'=> 'User',
+            'email'=> 'admin@business2business.space',
             'user_name'=> 'admin',
             'admin' => '1',
             // 'password' => Hash::make('12345678'),
@@ -56,130 +56,130 @@ class TestMatrixSeeder extends Seeder
             'status' => ReferalLink::STATUS_ACTIVE,
         ]);
 
-        $user = User::create([
-            'name'=> 'user',
-            'last_name'=> 1,
-            'email'=> "user1@b2b.com",
-            'user_name'=> 'user1',
-            'admin'=> '0',
-            // 'password' => Hash::make('123456789'),
-            'email_verified_at' => now(),
-            'status' => '1',
-            'buyer_id' => 1,
-            'binary_id' => 1,
-            'binary_side' => 'L'
-        ]);
+        // $user = User::create([
+        //     'name'=> 'user',
+        //     'last_name'=> 1,
+        //     'email'=> "user1@b2b.com",
+        //     'user_name'=> 'user1',
+        //     'admin'=> '0',
+        //     // 'password' => Hash::make('123456789'),
+        //     'email_verified_at' => now(),
+        //     'status' => '1',
+        //     'buyer_id' => 1,
+        //     'binary_id' => 1,
+        //     'binary_side' => 'L'
+        // ]);
 
-        $order = Order::create([
-            'user_id' => $user->id,
-            'amount' => 50,
-            'hash' => null,
-            'status' => '1',
-            'cyborg_id' => '1',
-            'membership_packages_id' => 1
-        ]);
+        // $order = Order::create([
+        //     'user_id' => $user->id,
+        //     'amount' => 50,
+        //     'hash' => null,
+        //     'status' => '1',
+        //     'cyborg_id' => '1',
+        //     'membership_packages_id' => 1
+        // ]);
 
-        /* Inversion::create([
-            'package_id' => 1,
-            'orden_id' => $order->id,
-            'user_id' => $user->id,
-            'status' => Inversion::STATUS_APPROVED,
-            'amount' => 50,
-            'type' => Inversion::TYPE_INITIAL_MATRIX
-        ]); */
+        // /* Inversion::create([
+        //     'package_id' => 1,
+        //     'orden_id' => $order->id,
+        //     'user_id' => $user->id,
+        //     'status' => Inversion::STATUS_APPROVED,
+        //     'amount' => 50,
+        //     'type' => Inversion::TYPE_INITIAL_MATRIX
+        // ]); */
 
-        $marketPurchased = MarketPurchased::create([
-            'user_id' => $user->id,
-            'cyborg_id' => 1,
-            'order_id' => $order->id
-        ]);
+        // $marketPurchased = MarketPurchased::create([
+        //     'user_id' => $user->id,
+        //     'cyborg_id' => 1,
+        //     'order_id' => $order->id
+        // ]);
 
-        ReferalLink::create([
-            'user_id' => $user->id,
-            'link_code' => Str::random(6),
-            'cyborg_id' => 1,
-            'right' => 1,
-            'left' => 1,
-            'status' => ReferalLink::STATUS_INACTIVE,
-        ]);
+        // ReferalLink::create([
+        //     'user_id' => $user->id,
+        //     'link_code' => Str::random(6),
+        //     'cyborg_id' => 1,
+        //     'right' => 1,
+        //     'left' => 1,
+        //     'status' => ReferalLink::STATUS_INACTIVE,
+        // ]);
 
-        /* ReferalLink::create([
-            'user_id' => $user->id,
-            'link_code' => Str::random(6),
-            'cyborg_id' => 2,
-            'right' => 0,
-            'left' => 0,
-            'status' => ReferalLink::STATUS_ACTIVE,
-        ]); */
+        // /* ReferalLink::create([
+        //     'user_id' => $user->id,
+        //     'link_code' => Str::random(6),
+        //     'cyborg_id' => 2,
+        //     'right' => 0,
+        //     'left' => 0,
+        //     'status' => ReferalLink::STATUS_ACTIVE,
+        // ]); */
 
-        $bonusService->generateFirstComission(20, $user, $order, $buyer = $user, $level = 2, $user->id);
+        // $bonusService->generateFirstComission(20, $user, $order, $buyer = $user, $level = 2, $user->id);
 
-        for($i = 3; $i < 33; $i++) {
+        // for($i = 3; $i < 33; $i++) {
 
-            $father = User::where('id', round($i / 2))->with('marketPurchased')->first();
+        //     $father = User::where('id', round($i / 2))->with('marketPurchased')->first();
 
-            $user = User::create([
-                'name'=> 'user',
-                'last_name'=> $i,
-                'email'=> "user{$i}@b2b.com",
-                'user_name'=> 'user'.$i,
-                'admin'=> '0',
-                // 'password' => Hash::make('123456789'),
-                'email_verified_at' => now(),
-                'status' => '1',
-                'buyer_id' => round($i / 2),
-                'binary_id' => round($i / 2),
-                'binary_side' => ($i % 2) == 0 ? 'R' : 'L',
-                'father_cyborg_purchased_id' => $father->marketPurchased->first()->id
-            ]);
+        //     $user = User::create([
+        //         'name'=> 'user',
+        //         'last_name'=> $i,
+        //         'email'=> "user{$i}@b2b.com",
+        //         'user_name'=> 'user'.$i,
+        //         'admin'=> '0',
+        //         // 'password' => Hash::make('123456789'),
+        //         'email_verified_at' => now(),
+        //         'status' => '1',
+        //         'buyer_id' => round($i / 2),
+        //         'binary_id' => round($i / 2),
+        //         'binary_side' => ($i % 2) == 0 ? 'R' : 'L',
+        //         'father_cyborg_purchased_id' => $father->marketPurchased->first()->id
+        //     ]);
 
-            Product::create([
-                'name'=> "prueba {$i}",
-                'user_id'=> $user->id,
-                'country'=> '0',
-                'document_id'=> '1',
-                'postal_code'=> '1',
-                'phone_number' => rand(10000000,99999999),
-                'status' => '1',
-                'state' => 'united',
-                'street' => '1',
-                'department' => $i
-            ]);
+        //     Product::create([
+        //         'name'=> "prueba {$i}",
+        //         'user_id'=> $user->id,
+        //         'country'=> '0',
+        //         'document_id'=> '1',
+        //         'postal_code'=> '1',
+        //         'phone_number' => rand(10000000,99999999),
+        //         'status' => '1',
+        //         'state' => 'united',
+        //         'street' => '1',
+        //         'department' => $i
+        //     ]);
 
-            $order = Order::create([
-                'user_id' => $user->id,
-                'amount' => 50,
-                'hash' => null,
-                'status' => '1',
-                'cyborg_id' => '1',
-                'membership_packages_id' => 1
-            ]);
+        //     $order = Order::create([
+        //         'user_id' => $user->id,
+        //         'amount' => 50,
+        //         'hash' => null,
+        //         'status' => '1',
+        //         'cyborg_id' => '1',
+        //         'membership_packages_id' => 1
+        //     ]);
 
-            $marketPurchased = MarketPurchased::create([
-                'user_id' => $user->id,
-                'cyborg_id' => 1,
-                'order_id' => $order->id
-            ]);
+        //     $marketPurchased = MarketPurchased::create([
+        //         'user_id' => $user->id,
+        //         'cyborg_id' => 1,
+        //         'order_id' => $order->id
+        //     ]);
 
-            $data = [
-                'user_id' => $user->id,
-                'link_code' => Str::random(6),
-                'cyborg_id' => 1,
-                'right' => 1,
-                'left' => 1,
-                'status' => ReferalLink::STATUS_INACTIVE,
-            ];
+        //     $data = [
+        //         'user_id' => $user->id,
+        //         'link_code' => Str::random(6),
+        //         'cyborg_id' => 1,
+        //         'right' => 1,
+        //         'left' => 1,
+        //         'status' => ReferalLink::STATUS_INACTIVE,
+        //     ];
 
-            if($i > 16) {
-                $data['right'] = 0;
-                $data['left'] = 0;
-                $data['status'] = ReferalLink::STATUS_ACTIVE;
-            }
+        //     if($i > 16) {
+        //         $data['right'] = 0;
+        //         $data['left'] = 0;
+        //         $data['status'] = ReferalLink::STATUS_ACTIVE;
+        //     }
 
-            ReferalLink::create($data);
+        //     ReferalLink::create($data);
 
-            $bonusService->generateFirstComission(20,$user, $order, $buyer = $user, $level = 2, $user->id);
+        //     $bonusService->generateFirstComission(20,$user, $order, $buyer = $user, $level = 2, $user->id);
 
-        }
+        // }
     }
 }
